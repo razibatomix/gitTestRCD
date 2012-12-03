69a712a1e9b421ffac3e6671ebeb71d26538f5be
Razib <razibdeb@atomixsystem.com>
2012-12-03 15:54:12 +0600

    Initial Commit

---
+++ gitTestRCD/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gitTestRCD
+//
+//  Created by Md Bashar on 12/3/12.
+//  Copyright (c) 2012 Atomix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gitTestRCD/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gitTestRCD
+//
+//  Created by Md Bashar on 12/3/12.
+//  Copyright (c) 2012 Atomix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
